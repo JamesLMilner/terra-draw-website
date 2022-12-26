edc623c52fe0a654ea496c56e72b8e19df5f6a99
James <jamesmilner22@hotmail.com>
2022-12-26 11:13:46 +0000

    Add download GeoJSON button

--- src/components/geojson-tab/GeoJSONTab.tsx
+++ src/components/geojson-tab/GeoJSONTab.tsx
@@ -1,20 +1,39 @@
 import { h } from "preact";
 import style from "./style.css";
-import { getHHMMSS } from "../../utils/dates";
-import { area, length } from "../../utils/geo";
 import { useMemo } from "preact/hooks";
 import { GeoJSONStoreFeatures } from "terra-draw/dist/store/store";
+import { useDownloadJSON } from "./useDownloadJSON";
 
 const GeoJSONTab = ({ features }: { features: GeoJSONStoreFeatures[] }) => {
-  const featureCollection = useMemo(() => {
-    return JSON.stringify({ type: "FeatureCollection", features }, null, 4);
-  }, [features]);
+  // Create a FeatureCollection when features changes
+  const featureCollection = useMemo(
+    () => ({
+      type: "FeatureCollection",
+      features,
+    }),
+    [features]
+  );
+
+  // Turn it into a string so it can be rendered,
+  // again only when features change
+  const featureCollectionString = useMemo(() => {
+    return JSON.stringify(featureCollection, null, 4);
+  }, [featureCollection]);
+
+  const downloadJSON = useDownloadJSON();
+
+  // Download to a file called terradraw.geojson
+  const downloadGeoJSON = () =>
+    downloadJSON(featureCollection, "terradraw.geojson");
 
   return (
     <div class={style.container}>
       <textarea class={style.geojson} readonly>
-        {featureCollection}
+        {featureCollectionString}
       </textarea>
+      <button class={style.download} onClick={downloadGeoJSON}>
+        Download GeoJSON
+      </button>
     </div>
   );
 };
---
+++ src/components/geojson-tab/useDownloadJSON.ts
@@ -0,0 +1,34 @@
+import { useCallback } from "preact/hooks";
+
+export function useDownloadJSON() {
+  const downloadJSON = useCallback(
+    (json: Record<string, any>, filename: string) => {
+      // Turn the JSON object into a string
+      const data = JSON.stringify(json);
+
+      // Pass the string to a Blob and turn it
+      // into an ObjectURL
+      const blob = new Blob([data], { type: "application/json" });
+      const jsonObjectUrl = URL.createObjectURL(blob);
+
+      // Create an anchor element, set it's
+      // href to be the Object URL we have created
+      // and set the download property to be the file name
+      // we want to set
+      const anchorEl = document.createElement("a");
+      anchorEl.href = jsonObjectUrl;
+      anchorEl.download = filename;
+
+      // There is no need to actually attach the DOM
+      // element but we do need to click on it
+      anchorEl.click();
+
+      // We don't want to keep a reference to the file
+      // any longer so we release it manually
+      URL.revokeObjectURL(jsonObjectUrl);
+    },
+    []
+  );
+
+  return downloadJSON;
+}
