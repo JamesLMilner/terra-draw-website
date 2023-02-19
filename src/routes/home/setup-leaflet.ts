a3a75ffae189f531afc518d468d1400a98ff8c21
James <jamesmilner22@hotmail.com>
2023-02-19 20:26:09 +0000

    fix max zoom being to high

--- src/routes/home/setup-leaflet.ts
+++ src/routes/home/setup-leaflet.ts
@@ -25,7 +25,7 @@ export function setupLeafletMap({
         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       subdomains: "abcd",
       minZoom: 3,
-      maxZoom: 17,
+      maxZoom: 16,
       ext: "png",
     } as any
   ).addTo(leafletMap);
