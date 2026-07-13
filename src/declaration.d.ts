5b5e141f8828942d642b226ea540c5349231af25
James Milner <jamesmilner22@hotmail.com>
2026-07-13 22:04:43 +0100

    chore: upgrade dependencies   (#40)

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -7,3 +7,17 @@ declare module "*.png" {
   const mapping: string;
   export default mapping;
 }
+
+declare module "preact-router/match" {
+  import * as preact from "preact";
+
+  interface LinkProps {
+    activeClassName?: string;
+    children?: preact.ComponentChildren;
+    href?: string;
+  }
+
+  export function Link(
+    props: LinkProps & preact.JSX.AnchorHTMLAttributes<HTMLAnchorElement>
+  ): preact.VNode;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import './style/index.css';
 import App from './components/app';
 import { hydrate, prerender as ssr } from 'preact-iso';
+import { h } from 'preact';
 
 if (typeof window !== 'undefined') {
-    hydrate(<App />, document.getElementById('app'));
+    hydrate(<App />, document.getElementById('app') as HTMLElement);
 }
 
-export async function prerender(data) {
-    return await ssr(<App {...data} />);
+export async function prerender() {
+    return await ssr(<App />);
 }
